ad30d187f7df4bfc09ff3047c429e5cd4701c70c
amanKgupta <amangupta.ngp@gmail.com>
2022-08-24 23:02:56 +0530

    Initial commit for springit

---
+++ src/main/java/com/vega/springit/AmanApplication.java
@@ -0,0 +1,13 @@
+package com.vega.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmanApplication.class, args);
+	}
+
+}
