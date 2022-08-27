c62f29a97d829b4d3f45fcedc4818fe865dc35fe
amanKgupta <amangupta.ngp@gmail.com>
2022-08-27 09:54:43 +0530

    accociation mapping added to link and comment domain

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -3,9 +3,7 @@ package com.vega.springit.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -16,4 +14,6 @@ public class Comment {
     private Long id;
     private String body;
 
+    @ManyToOne
+    private Link link;
     }
--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -2,10 +2,14 @@ package com.vega.springit.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -15,7 +19,11 @@ public class Link {
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String  url;
 
+    @OneToMany(mappedBy = "link")
+   private List<Comment> comments = new ArrayList<>();
 }
